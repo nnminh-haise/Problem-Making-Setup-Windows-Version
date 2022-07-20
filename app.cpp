6917a5c2d1f347d18e1cd6e967a9102758aefb3d
tuinnguyen <nnminh.sam.1803@gmail.com>
2022-07-20 21:21:05 +0700

    Upload | Upload Problem-set version 1.0

---
+++ app.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#include "libraries/functions.h"
+#include "libraries/powershell-command.h"
+#include "libraries/classes.h"
+#include "libraries/json-setting.h"
+
+using namespace std;
+
+const int date = 0;
+
+JSON setting;
+
+void makeInput(const int testnumber, ofstream &cout) {
+    return;
+}
+
+void makeOutput(ifstream &cin, ofstream &cout) {
+    return;
+}
+
+int main() {
+    srand(time(NULL));
+
+    cerr << "Start making testcases!\n\n";
+
+    string ID = generateID(date);
+    cerr << "Problem ID: " << ID << endl;
+    cerr << "\n";
+
+    cerr << "Reading settings!\n";
+    setting.readJson("setting.json");
+
+    cerr << "\n";
+
+    vector <string> files;
+
+    for (int currentTest = 1; currentTest <= setting.numberOfTestcase; ++currentTest) {
+        cerr << "Making test " << currentTest << "!\n";
+
+        const string inFileName = integerToString(currentTest) + ".in";
+        const string outFileName = integerToString(currentTest) + ".out";
+
+        ifstream inputFile;
+        ofstream inFile, outFile;
+        inFile.open(inFileName);
+        outFile.open(outFileName);
+        inputFile.open(inFileName);
+
+        makeInput(currentTest, inFile);
+        makeOutput(inputFile, outFile);
+
+        inFile.close();
+        outFile.close();
+        inputFile.close();
+        files.push_back(inFileName);
+        files.push_back(outFileName);
+    }
+
+    cerr << "Compressing testcases into \"testcases.zip\"" << endl;
+    Compress compressFile;
+    compressFile.createArchiveFile(files, "testcases.zip");
+    executeFile("zip.ps1");
+    cerr << "Complete compression!\n";
+
+    cerr << "\n";
+
+    if (setting.removeClusterFiles == true) {
+        cerr << "Removing *.in and *.out files\n";
+        for (string file: files)
+            removeFile(file);
+
+        removeFile("zip.ps1");
+        removeFile("app.exe");
+        cerr << "Complete removing files!\n";
+
+        cerr << "\n";
+    }
+
+    cerr << "Program complete!\n";
+
+    return 0;
+}
+
---
+++ statement.md
@@ -0,0 +1,24 @@
+#
+
+***Yêu cầu:*** 
+
+## Input
+
+## Constraints
+
+## Subtasks
+
+- Subtask $1$ ($25\%$ số điểm): có
+- Subtask $2$ ($75\%$ số điểm): không có ràng buộc gì thêm.
+
+## Output
+
+## Sample Input
+
+```
+```
+
+## Sample Output
+
+```
+```
